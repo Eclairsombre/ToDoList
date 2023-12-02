b13e30b94d50f72799e068664f8afd0284b3be1f
Eclairsombre <alexandre.thny@gmail.com>
2023-12-02 22:46:44 +0100

    Simple ToDoList in React

---
+++ src/TaskForm.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+export default function TaskForm({ onAdd }) {
+    const [taskName, setTaskName] = useState("");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!taskName) return;
+        onAdd(taskName);
+        setTaskName("");
+        
+ 
+    }
+  return (
+    <form onSubmit={handleSubmit}>
+      <button> + </button>
+      <input
+        type="text"
+        value={taskName}
+        placeholder="Add a new task"
+        onChange={(e) => setTaskName(e.target.value)}
+      />
+    </form>
+  );
+}
---
+++ src/ToDoList.jsx
@@ -0,0 +1,74 @@
+
+import React, { useEffect, useState } from 'react'
+import './App.css'
+import Task from './Task'
+import TaskForm from './TaskForm'
+
+function ToDoList() {
+  const [tasks, setTasks] = useState([])
+
+  function addTask(name) {
+    setTasks(prev => {
+      return [...prev, { name: name, done: false }]
+    });
+  }
+  function removeTask(taskIndex) {
+    setTasks(prev => {
+      const newTasks = prev.filter((task, index) => index !== taskIndex);
+      localStorage.setItem('tasks', JSON.stringify(newTasks));
+      return newTasks;
+    });
+  }
+
+
+  useEffect(() => {
+    if (tasks.length === 0) return ;
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+  },[tasks]);
+
+  useEffect(() => {
+   const tasks =JSON.parse(localStorage.getItem('tasks') );
+   setTasks(tasks);
+  },[]);
+
+  function updateTaskDone(taskIndex,done){
+    setTasks(prev => {
+      return prev.map((task,index) => {
+        if (index === taskIndex){
+          return {...task,done}
+        }
+        return task;
+      })
+    })
+  }
+
+  const numberOfTasks = tasks.length;
+  const numberOfTasksDone = tasks.filter(task => task.done).length;
+  const message = numberOfTasksDone === numberOfTasks ? "All tasks done !" : "Keep going !";
+
+
+  
+  
+  return (
+
+
+    <div>
+
+      <h1>ToDoList</h1>
+      <h2>{numberOfTasksDone}/{numberOfTasks} Complete</h2>
+      <h3>{message}</h3>
+
+      <TaskForm onAdd={addTask} />
+      {tasks.map((task ,index) => (
+        <Task {...task}
+        onTrash={() => removeTask(index)}
+         onToggle={done => updateTaskDone(index,done)} />
+      ))
+      }
+
+
+    </div>
+  )
+}
+
+export default ToDoList
